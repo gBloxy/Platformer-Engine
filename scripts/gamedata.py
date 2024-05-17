f89e75d48277a2ca6429aba785404248c454283a
g_Bloxy <121670440+gBloxy@users.noreply.github.com>
2024-05-17 18:35:43 +0200

    Delete scripts directory - re organisation

--- scripts/gamedata.py
+++
@@ -1,9 +0,0 @@
-
-from os.path import join
-
-from .core import read_json
-
-
-class GameDataManager():
-    def __init__(self, path):
-        self.entities = read_json(join(path, 'entities.json'))
